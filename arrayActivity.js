910e3b3f723c49fb78a8389fa86b36cc8fab2f28
divinecallejo <callejodivinerachelle@gmail.com>
2025-03-30 22:22:17 +0800

    Add files via upload

---
+++ arrayActivity.js
@@ -0,0 +1,17 @@
+// Create an array of favorite singers
+let favSingers = ["Taylor Swift", "Niki", "Bruno Mars"];
+
+// Log the first singer using array index notation
+console.log("First favorite singer:", favSingers[0]);
+
+// Create an array of favorite numbers
+let favNumbers = [5, 9, 20, 22];
+
+// Create an array with mixed data types
+let mixedArray = ["string", ["otherarray"], 123, true];
+
+// Access and log each item in the mixed array
+console.log("First item in mixedArray:", mixedArray[0]);
+console.log("Second item in mixedArray:", mixedArray[1]);
+console.log("Third item in mixedArray:", mixedArray[2]);
+console.log("Fourth item in mixedArray:", mixedArray[3]);
